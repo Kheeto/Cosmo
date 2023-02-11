3076ec63d31f723efe649aa7e5781d2bdfbf1ab8
Kheeto <70845116+Kheeto@users.noreply.github.com>
2023-02-11 13:20:35 +0100

    Create AmmoText.cs

---
+++ Scripts/Weapons/AmmoText.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AmmoText : MonoBehaviour
+{
+    [SerializeField] private List<Missile> missiles = new List<Missile>();
+    [SerializeField] private List<Cannon> cannons = new List<Cannon>();
+
+    [Header("References")]
+    [SerializeField] private TMP_Text ammoText;
+
+    public void UpdateAmmoText()
+    {
+        int cannonAmmo = 0;
+        foreach (Cannon c in cannons)
+        {
+            cannonAmmo += c.GetCurrentAmmo();
+        }
+        int missileCount = 0;
+        foreach (Missile m in missiles)
+        {
+            if (!m.wasLaunched) missileCount++;
+        }
+
+        ammoText.text = "CNN " + cannonAmmo + "\nMSL " + missileCount;
+    }
+}
