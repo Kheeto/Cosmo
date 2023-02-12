aa7b91124d35e204c4c54c60def0954932c6b94b
Kheeto <70845116+Kheeto@users.noreply.github.com>
2023-02-12 19:36:22 +0100

    Create MenuManager.cs

---
+++ Scripts/Menu/MenuManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject optionsMenu;
+    [SerializeField] private GameObject levelsMenu;
+
+    private void Start()
+    {
+        OpenMainMenu();
+    }
+
+    public void OpenMainMenu()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+        levelsMenu.SetActive(false);
+    }
+
+    public void OpenOptionsMenu()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+        levelsMenu.SetActive(false);
+    }
+
+    public void OpenLevelsMenu()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+        levelsMenu.SetActive(true);
+    }
+
+    public void LoadLevel(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
