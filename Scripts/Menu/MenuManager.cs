7770ab65598271be525b8830730c6f3884956fd9
Kheeto <70845116+Kheeto@users.noreply.github.com>
2023-02-14 18:00:14 +0100

    Update MenuManager.cs

--- Scripts/Menu/MenuManager.cs
+++ Scripts/Menu/MenuManager.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    [Header("Graphics Settings")]
+    [SerializeField] private UniversalRenderPipelineAsset graphicsHigh;
+    [SerializeField] private UniversalRenderPipelineAsset graphicsLow;
+    [SerializeField] private TMP_Text graphicsText;
+
     [Header("References")]
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject optionsMenu;
     [SerializeField] private GameObject levelsMenu;
     [SerializeField] private Animator animator;
+
+    [SerializeField] private List<Slider> sliders = new List<Slider>();
+    [SerializeField] private List<TMP_Text> sliderTexts = new List<TMP_Text>();
 
     private void Start()
     {
@@ -46,5 +58,24 @@ public class MenuManager : MonoBehaviour
     public void LoadLevel(int index)
     {
         SceneManager.LoadScene(index);
+    }
+
+    public void OnGraphicsSettingUpdated()
+    {
+        if (GraphicsSettings.renderPipelineAsset == graphicsHigh)
+        {
+            GraphicsSettings.renderPipelineAsset = graphicsLow;
+            graphicsText.text = "LOW";
+        }
+        else if (GraphicsSettings.renderPipelineAsset == graphicsLow)
+        {
+            GraphicsSettings.renderPipelineAsset = graphicsHigh;
+            graphicsText.text = "HIGH";
+        }
+    }
+
+    public void OnSliderValueUpdated(int index)
+    {
+        sliderTexts[index].text = sliders[index].value.ToString("0.##");
     }
 }
