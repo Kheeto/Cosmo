2b95be6fa496d33ed48ce05c6af534adf0d58177
Kheeto <kheetoxd@gmail.com>
2023-10-12 17:26:39 +0200

    Face render mask
    This setting allows you to only render one face of the planet, for faster rendering times

--- Scripts/Planets/Planet.cs
+++ Scripts/Planets/Planet.cs
@@ -7,6 +7,8 @@ public class Planet : MonoBehaviour {
     [Range(2, 256)]
     public int resolution = 10;
     public bool autoUpdate = true;
+    public enum FaceRenderMask { All, Top, Bottom, Left, Right, Front, Back }
+    public FaceRenderMask faceRenderMask;
 
     public ShapeSettings shapeSettings;
     public ColourSetttings colourSettings;
@@ -52,6 +54,8 @@ public class Planet : MonoBehaviour {
             }
 
             terrainFaces[i] = new TerrainFace(shapeGenerator, meshFilters[i].sharedMesh, resolution, directions[i]);
+            bool renderFace = faceRenderMask == FaceRenderMask.All || (int)faceRenderMask - 1 == i;
+            meshFilters[i].gameObject.SetActive(renderFace);
         }
     }
 
@@ -78,9 +82,12 @@ public class Planet : MonoBehaviour {
 
     private void GenerateMesh()
     {
-        foreach (TerrainFace face in terrainFaces)
+        for (int i = 0; i < 6; i++)
         {
-            face.ConstructMesh();
+            if (meshFilters[i].gameObject.activeSelf)
+            {
+                terrainFaces[i].ConstructMesh();
+            }
         }
     }
 
